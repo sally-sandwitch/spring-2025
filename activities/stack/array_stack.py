2ca9193fda0c43626d8b0ccd21a5cab8c551bcd1
Adriana <127362145+sally-sandwitch@users.noreply.github.com>
2025-05-02 14:16:11 -0700

    feat: Add my implementation of an array-based stack

--- activities/stack/array_stack.py
+++ activities/stack/array_stack.py
@@ -3,13 +3,28 @@
 class ArrayStack:
 
     def __init__(self):
-        pass
+        self._data = [None]
+        self._size = 0
+
 
     def push(self, item):
-        pass
+        if self._size == len(self._data):
+            self._expand()
+        self._data[self._size] = item
+        self._size += 1
 
     def pop(self):
-        pass
+        self._size -= 1
+        return self._data[self._size]
 
     def is_empty(self):
-        pass
+        return self._size == 0
+    
+    def _expand(self):
+        n = len(self._data)
+        new_data = [None] * n * 2
+        for i in range(n - 1):
+            new_data[i] = self._data[(self._front + i) % n]
+        self._front = 0
+        self._back = n - 1
+        self._data = new_data
